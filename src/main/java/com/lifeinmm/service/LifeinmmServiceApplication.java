fc2a6b2098c58162652d5986e7df2d50c9576eba
abhinavgoel <abhinav.goel@gmail.com>
2018-02-22 00:48:29 +0100

    bootstrap

---
+++ src/main/java/com/lifeinmm/service/LifeinmmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifeinmm.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class LifeinmmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeinmmServiceApplication.class, args);
+	}
+}
